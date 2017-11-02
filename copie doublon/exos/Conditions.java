06ace741e849c6148a6af3cd38330b788658f2c3
Hlegoff <helena.le.goff@hotmail.fr>
2017-11-02 15:58:38 +0100

    add new hierarchy

---
+++ copie doublon/exos/Conditions.java
@@ -0,0 +1,98 @@
+public class Conditions {
+
+	public static void main(String[] args)
+	
+	{
+
+		///////// ELSE IF ////////
+		
+//int i = 10;
+//if (i < 0)
+//  System.out.println("le nombre est négatif");
+//else
+//  System.out.println("le nombre est positif");
+
+/* La condition du if est remplie si le nombre est strictement 
+ * négatif, ce qui n'est pas le cas ici puisque nous allons 
+ * le mettre à 0. Le code contenu dans la clause else est donc exécuté si le nombre 
+ * est égal ou strictement supérieur à 0. Il nous suffit d'ajouter une condition à 
+ * l'intérieur de la clause else */
+		
+//int i = 0;
+//if (i < 0)
+//{
+//  System.out.println("Ce nombre est négatif !");
+//} 
+//else
+//{
+//  if(i == 0)
+//    System.out.println("Ce nombre est nul !");
+// 
+//  else
+//    System.out.println("Ce nombre est positif !");
+// 
+//}
+//	}
+//}
+
+//int i = 0;
+//if (i < 0)
+//  System.out.println("Ce nombre est négatif !");      
+//  
+//else if(i > 0)
+//  System.out.println("Ce nombre est positif !");           
+// 
+//else  
+//  System.out.println("Ce nombre est nul !");
+		
+//		int i = 49;
+//		if(i < 100 || i > 100) 
+//		  System.out.println("Le nombre est bien dans l'intervalle.");
+//		else
+//		  System.out.println("Le nombre n'est pas dans l'intervalle.");
+
+
+			/////////// SWITCH ////////////
+
+//		int note = 8; //On imagine que la note maximale est 20
+//
+//		switch (note)
+//		{
+//		  case 0:
+//		    System.out.println("Ouch !");
+//		    break;
+//		  case 10:
+//		    System.out.println("Vous avez juste la moyenne.");
+//		    break;
+//		  case 20:
+//		    System.out.println("Parfait !");
+//		    break;
+//		  default:
+//		    System.out.println("Il faut davantage travailler.");
+//		}
+			
+		
+//		String chaine = "FFHFGG";
+//
+//		switch(chaine) {
+//		  case "Bonjour":
+//		    System.out.println("Yes !");
+//		    break;
+//		  case "Bonsoir":
+//		    System.out.println("Bonsoir monsieur !");
+//		    break;
+//		  default:
+//		    System.out.println("Bonjoir ! :p");
+//		}
+		
+		///////////La condition ternaire///////////
+		
+		int x = 10, y = 20;
+		int max = (x < y) ? y : x ; //Maintenant, max vaut 20
+		
+		
+		
+		
+	}
+	}
+
